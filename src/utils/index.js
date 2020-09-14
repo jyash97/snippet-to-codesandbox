6b1f23db34fe96955ade665c5facfb9bbaf5ef98
lakhansamani <lakhan.m.samani@gmail.com>
2020-09-15 00:10:18 +0530

    fix: rollup config for parser

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,14 +1,14 @@
-// import { parse, print } from 'recast';
-// import parser from 'recast/parsers/babylon';
+import { parse, print } from 'recast';
+import parser from 'recast/parsers/babylon';
 import { getReactFiles } from '../templates/react';
 
 const getTemplateFiles = ({ template, content: code }) => {
-	// const ast = parse(code, {
-	// 	parser,
-	// });
-	// ast.program.body = ast.program.body.filter((item) => item.type !== 'ImportDeclaration');
+	const ast = parse(code, {
+		parser,
+	});
+	ast.program.body = ast.program.body.filter((item) => item.type !== 'ImportDeclaration');
 
-	// const content = print(ast).code;
+	const content = print(ast).code;
 	switch (template) {
 		case 'react': {
 			return getReactFiles({ content: code, import: '' });
