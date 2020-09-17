4833341775852d715908308bd032de30204a85b3
Yash Joshi <jyash97@gmail.com>
2020-09-17 19:37:44 +0530

    Add prettier & its config for testing

--- src/__tests__/react/index.test.js
+++ src/__tests__/react/index.test.js
@@ -1,8 +1,11 @@
+import prettier from 'prettier/standalone';
+import babylon from 'prettier/parser-babel';
 const { ReactParser } = require('../../templates/react/parser');
 
 // TODO: Add tests use prettier to test formatting of the code.
 describe('React has DOM Rendering', () => {
 	test('Basic Test', () => {
+		console.log(global.PRETTIER_CONFIG);
 		return expect(true).toBeTruthy();
 	});
 });
